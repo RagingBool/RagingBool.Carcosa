bd160330228352a11b54bb57a2e8a1de4a491546
untrots <untrots@gmail.com>
2015-03-30 17:25:39 +0300

    Creating a Carcosa instance and managing its lifespan.

--- projects/RagingBool.Carcosa.App_cs/App.xaml.cs
+++ projects/RagingBool.Carcosa.App_cs/App.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Epicycle.Commons.FileSystem;
+using Epicycle.Commons.Time;
 using System.Windows;
 
 namespace RagingBool.Carcosa.App
 {
+    using RagingBool.Carcosa.Core;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private SystemClock _clock;
+        private StandardFileSystem _fileSystem;
+        private Carcosa _carcosa;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            _clock = new SystemClock();
+            _fileSystem = StandardFileSystem.Instance;
+
+            _carcosa = new Carcosa(_clock, _fileSystem);
+        }
+
+        public ICarcosa Carcosa
+        {
+            get { return _carcosa; }
+        }
     }
 }
--- projects/RagingBool.Carcosa.App_cs/MainWindow.xaml.cs
+++ projects/RagingBool.Carcosa.App_cs/MainWindow.xaml.cs
@@ -1,28 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RagingBool.Carcosa.App
 {
+    using RagingBool.Carcosa.Core;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ICarcosa _carcosa;
+
         public MainWindow()
         {
+            _carcosa = ((App)Application.Current).Carcosa;
+
             InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            _carcosa.Start();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            _carcosa.Stop();
         }
     }
 }
