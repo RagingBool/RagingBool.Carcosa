64822aab2b6b5001b6d5f2759abbf5c0396df506
untrots <untrots@gmail.com>
2015-03-31 18:42:19 +0300

    +SceneBasics

---
+++ projects/RagingBool.Carcosa.Core_cs/Stage/Scenes/SceneBasics.cs
@@ -0,0 +1,75 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RagingBool.Carcosa.Core.Stage.Controller;
+
+namespace RagingBool.Carcosa.Core.Stage.Scenes
+{
+    internal abstract class SceneBase : IScene
+    {
+        private const int NumberOfControls = 8;
+        private const int ControlDefaultRawValue = 128;
+
+        private int[] _rawControls;
+        private double[] _controls;
+
+        public SceneBase()
+        {
+            _rawControls = new int[NumberOfControls];
+            _controls = new double[NumberOfControls];
+
+            for(var i = 0; i < NumberOfControls; i++)
+            {
+                SetControl(i, ControlDefaultRawValue);
+            }
+        }
+
+        public abstract void Enter();
+        public abstract void Exit();
+        public abstract void Update(double dt);
+        
+        private void SetControl(int id, int rawValue)
+        {
+            _rawControls[id] = rawValue;
+            _controls[id] = rawValue / 254.0;
+        }
+
+        public int GetControlRaw(int id)
+        {
+            return _rawControls[id];
+        }
+
+        public double GetControl(int id)
+        {
+            return _controls[id];
+        }
+
+        public abstract void HandleSubsceneChange(int newSubscene);
+        public abstract void HandleLightDrumEvent(LightDrumEventArgs eventArgs);
+
+        public void HandleControlParameterValueChange(ControlParameterValueChangeEventArgs eventArgs)
+        {
+            SetControl(eventArgs.ControlParameterId, eventArgs.NewValue);
+        }
+    }
+}
