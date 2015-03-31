66968adbf2dc6c2127fd218e63a9dad5ad28e5de
untrots <untrots@gmail.com>
2015-03-31 09:59:01 +0300

    +IControllerMode

---
+++ projects/RagingBool.Carcosa.Core_cs/Stage/Controller/IControllerMode.cs
@@ -0,0 +1,32 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+using RagingBool.Carcosa.Devices;
+
+namespace RagingBool.Carcosa.Core.Stage.Controller
+{
+    internal interface IControllerMode
+    {
+        void Enter();
+        void Exit();
+        void Update();
+
+        void ProcessButtonEventHandler(ButtonEventArgs e);
+        void ProcessControllerChangeEvent(ControllerChangeEventArgs e);
+    }
+}
