f0aa1b1da0c2099465182504d1d8cbc9a04a1b0f
untrots <untrots@gmail.com>
2015-04-01 15:40:34 +0300

    ManualScene: finalizing the scene

--- projects/RagingBool.Carcosa.Core_cs/Stage/Scenes/ManualScene.cs
+++ projects/RagingBool.Carcosa.Core_cs/Stage/Scenes/ManualScene.cs
@@ -26,14 +26,20 @@ namespace RagingBool.Carcosa.Core.Stage.Scenes
     {
         private readonly LightSetup _lightSetup;
 
+        private int _monoState;
+        private bool _isOff;
+        private bool _isFull;
+
         public ManualScene(LightSetup lightSetup)
         {
             _lightSetup = lightSetup;
+            _monoState = 0;
         }
 
         public override void Enter()
         {
-
+            _isOff = false;
+            _isFull = false;
         }
 
         public override void Exit()
@@ -43,18 +49,54 @@ namespace RagingBool.Carcosa.Core.Stage.Scenes
 
         public override void Update(double dt)
         {
-            var saturation = GetControl(3);
-            var intensity = GetControl(7);
+            UpdateRgbLights();
+            UpdateMonoStrips();
+        }
 
+        private void UpdateRgbLights()
+        {
+            var saturation = !_isFull ? GetControl(3) : 0.0;
+            var intensity = !_isOff ? (!_isFull ? GetControl(7) : 1) : 0.0;
+            
             var hue0 = GetControl(0);
-            var hue1 = GetControl(1);
-            var hue2 = GetControl(4);
+            var hue1 = GetControl(4);
+            var hue2 = GetControl(1);
             var hue3 = GetControl(5);
+            var hue4 = GetControl(2);
+            var hue5 = GetControl(6);
 
-            SetRgbLightToHsi(_lightSetup.RgbStrips[0], hue0, saturation, intensity);
-            SetRgbLightToHsi(_lightSetup.RgbStrips[1], hue1, saturation, intensity);
-            SetRgbLightToHsi(_lightSetup.RgbLights[0], hue2, saturation, intensity);
-            SetRgbLightToHsi(_lightSetup.RgbLights[1], hue3, saturation, intensity);
+            SetRgbLightToHsi(_lightSetup.RgbLights[0], hue0, saturation, intensity);
+            SetRgbLightToHsi(_lightSetup.RgbLights[1], hue1, saturation, intensity);
+            SetRgbLightToHsi(_lightSetup.RgbLights[2], hue2, saturation, intensity);
+            SetRgbLightToHsi(_lightSetup.RgbLights[3], hue3, saturation, intensity);
+            SetRgbLightToHsi(_lightSetup.RgbStrips[0], hue4, saturation, intensity);
+            SetRgbLightToHsi(_lightSetup.RgbStrips[1], hue5, saturation, intensity);
+        }
+
+        private void UpdateMonoStrips()
+        {
+            if(_isOff || _isFull)
+            {
+                var intensity = _isOff ? 0 : 1;
+
+                foreach(var light in _lightSetup.MonoStrips)
+                {
+                    light.Intensity = intensity;
+                }
+
+                return;
+            }
+
+            var intensity0 = (_monoState & 0x04) != 0 ? 1 : 0;
+            var intensity1 = (_monoState & 0x02) != 0 ? 1 : 0;
+            var intensity2 = (_monoState & 0x01) != 0 ? 1 : 0;
+
+            _lightSetup.MonoStrips[0].Intensity = intensity0;
+            _lightSetup.MonoStrips[1].Intensity = intensity1;
+            _lightSetup.MonoStrips[2].Intensity = intensity2;
+            _lightSetup.MonoStrips[3].Intensity = intensity2;
+            _lightSetup.MonoStrips[4].Intensity = intensity1;
+            _lightSetup.MonoStrips[5].Intensity = intensity0;
         }
 
         public override void HandleSubsceneChange(int newSubscene)
@@ -64,7 +106,18 @@ namespace RagingBool.Carcosa.Core.Stage.Scenes
 
         public override void HandleLightDrumEvent(LightDrumEventArgs eventArgs)
         {
-
+            switch (eventArgs.DrumId)
+            {
+                case 0:
+                    _monoState = (_monoState + 1) % 8;
+                    break;
+                case 2:
+                    _isOff = eventArgs.TriggerType == ButtonTriggerType.Pressed;
+                    break;
+                case 3:
+                    _isFull = eventArgs.TriggerType == ButtonTriggerType.Pressed;
+                    break;
+            }
         }
 
         private void SetRgbLightToHsi(IRgbLight rgbLight, double hue, double saturation, double intensity)
