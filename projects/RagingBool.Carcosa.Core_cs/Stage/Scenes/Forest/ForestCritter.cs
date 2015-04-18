4d020ec771ab45223c861db212fd4aa0add61766
untrots <untrots@gmail.com>
2015-04-18 23:39:21 +0300

    ForestCritter: adding slight hue oscillation and tweaking parameters

--- projects/RagingBool.Carcosa.Core_cs/Stage/Scenes/Forest/ForestCritter.cs
+++ projects/RagingBool.Carcosa.Core_cs/Stage/Scenes/Forest/ForestCritter.cs
@@ -32,6 +32,8 @@ namespace RagingBool.Carcosa.Core.Stage.Scenes.Forest
 
         private readonly Heartbeat _heartbeat;
         private readonly Oscillator _breathingOsc;
+        private readonly Oscillator _hueOsc1;
+        private readonly Oscillator _hueOsc2;
 
         public ForestCritter(ForestEnvironment environment, IRgbLight body, IEnumerable<IRgbLight> eyes)
         {
@@ -55,6 +57,14 @@ namespace RagingBool.Carcosa.Core.Stage.Scenes.Forest
             _breathingOsc = new Oscillator();
             _breathingOsc.Function = Oscillator.FunctionType.Sin;
             _breathingOsc.Frequency = 0.4;
+
+            _hueOsc1 = new Oscillator();
+            _hueOsc1.Function = Oscillator.FunctionType.Sin;
+            _hueOsc1.Frequency = 0.05;
+
+            _hueOsc2 = new Oscillator();
+            _hueOsc2.Function = Oscillator.FunctionType.Sin;
+            _hueOsc2.Frequency = 0.31;
         }
 
         public double Vitality { get; set; }
@@ -71,16 +81,21 @@ namespace RagingBool.Carcosa.Core.Stage.Scenes.Forest
         {
             _heartbeat.Update(dt);
             _breathingOsc.Update(dt);
+            _hueOsc1.Update(dt);
+            _hueOsc2.Update(dt);
 
             var heartbeat = _heartbeat.Value;
 
-            var baseIntensity = 0.8;
-            var intensity = baseIntensity + _breathingOsc.Value * 0.1 + heartbeat * 0.05;
+            var baseHue = PrimaryHue;
+            var hue = baseHue + (_hueOsc1.Value - 0.5) * 0.1 + (_hueOsc2.Value - 0.5) * 0.04;
+
+            var baseIntensity = 0.6 + _environment.Magic / 5;
+            var intensity = baseIntensity + _breathingOsc.Value * 0.1 + heartbeat * 0.02;
 
             var baseSaturation = _environment.Magic;
             var saturation = baseSaturation + heartbeat * 0.05;
 
-            LightUtils.SetRgbLightToHsi(_body, PrimaryHue, saturation, intensity);
+            LightUtils.SetRgbLightToHsi(_body, hue, saturation, intensity);
         }
     }
 }
