355748f652f9fb1cccc36c5130a55c6de0c27221
untrots <untrots@gmail.com>
2015-04-18 22:00:43 +0300

    +PhaseGenerator

---
+++ projects/RagingBool.Carcosa.Core_cs/Stage/Scenes/Signal/PhaseGenerator.cs
@@ -0,0 +1,42 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+using System;
+
+namespace RagingBool.Carcosa.Core.Stage.Scenes.Signal
+{
+    public sealed class PhaseGenerator
+    {
+        public PhaseGenerator()
+        {
+            Phase = 0;
+            Frequency = 0;
+        }
+
+        public double Phase { get; private set; }
+
+        public double Frequency { get; set; }
+
+        public void Update(double dt)
+        {
+            var advPhase = Phase + (dt * Frequency);
+
+            Phase = (advPhase <= 1) ? advPhase : (advPhase - Math.Floor(advPhase));
+        }
+    }
+}
