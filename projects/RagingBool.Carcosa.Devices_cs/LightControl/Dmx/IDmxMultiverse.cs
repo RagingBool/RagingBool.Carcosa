d11152e551959bd08080ed4d509278f281162158
untrots <untrots@gmail.com>
2015-07-07 14:57:37 +0300

    +IDmxMultiverse; +IOpcDevice

---
+++ projects/RagingBool.Carcosa.Devices_cs/LightControl/Dmx/IDmxMultiverse.cs
@@ -0,0 +1,34 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+namespace RagingBool.Carcosa.Devices.LightControl
+{
+    /// <summary>
+    /// A device that controls several indexed DMX512 universes. It only allows sending DMX messages.
+    /// More information about the protocol can be found here: https://en.wikipedia.org/wiki/DMX512
+    /// </summary>
+    public interface IDmxMultiverse : IDevice
+    {
+        /// <summary>
+        /// Sends a whole frame of DMX512 data
+        /// </summary>
+        /// <param name="universe">The index of the DMX512 universe.</param>
+        /// <param name="values">The DMX channel data. Each byte represents a channel. Expected to be exactly 512.</param>
+        void SendFrame(int universe, byte[] values);
+    }
+}
---
+++ projects/RagingBool.Carcosa.Devices_cs/LightControl/Opc/IOpcDevice.cs
@@ -0,0 +1,38 @@
+﻿// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+namespace RagingBool.Carcosa.Devices.LightControl
+{
+    /// <summary>
+    /// A lighting control device that communicates using the Open Pixel Control (OPC) protocol.
+    /// More information about OPC can be found here: http://openpixelcontrol.org/
+    /// </summary>
+    public interface IOpcDevice : IDevice
+    {
+        /// <summary>
+        /// Send a frame.
+        /// </summary>
+        /// <param name="channel">The channel to use.</param>
+        /// <param name="rgbValues">The RGB data to use. Each 3 bytes are assumed to be a 24-bit RGB color.</param>
+        void SendRgbFrame(byte channel, byte[] rgbValues);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="channel">The channel to use.</param>
+        /// <param name="systemId">a 16-bit ID of the intended system.</param>
+        /// <param name="commandData">The sytem-specific command.</param>
+        void SendSystemExclusive(byte channel, ushort systemId, byte[] commandData);
+    }
+}
