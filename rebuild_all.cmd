6d0b5dd03fe89fc6d8724129787d7f45b1b5d122
untrots <untrots@gmail.com>
2015-09-14 22:27:43 +0300

    Dropping .NET 4.0 support

--- rebuild_all.cmd
+++ rebuild_all.cmd
@@ -1,8 +1,6 @@
 @echo off
 
 cd projects
-msbuild RagingBool.Carcosa.net40.sln /t:Clean,Build /p:Configuration=Debug
-msbuild RagingBool.Carcosa.net40.sln /t:Clean,Build /p:Configuration=Release
 msbuild RagingBool.Carcosa.net45.sln /t:Clean,Build /p:Configuration=Debug
 msbuild RagingBool.Carcosa.net45.sln /t:Clean,Build /p:Configuration=Release
 
