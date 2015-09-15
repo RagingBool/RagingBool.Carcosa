a4bfdb5e76537719dab0efadbe00ba54ade12f91
untrots <untrots@gmail.com>
2015-09-15 13:43:00 +0300

    App.xaml.cs: +Clock

--- projects/RagingBool.Carcosa.App_cs/App.xaml.cs
+++ projects/RagingBool.Carcosa.App_cs/App.xaml.cs
@@ -1,4 +1,22 @@
-﻿using Epicycle.Commons.FileSystem;
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+using Epicycle.Commons.FileSystem;
 using Epicycle.Commons.Time;
 using System.Windows;
 using System;
@@ -32,9 +50,8 @@ namespace RagingBool.Carcosa.App
             _carcosa = new Carcosa(_clock, _fileSystem, workspacePath);
         }
 
-        public ICarcosa Carcosa
-        {
-            get { return _carcosa; }
-        }
+        public IClock Clock { get { return _clock; } }
+
+        public ICarcosa Carcosa { get { return _carcosa; } }
     }
 }
