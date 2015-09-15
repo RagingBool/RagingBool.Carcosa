7d6444830c6ad3cb9a6239e0ca134b901b7fc572
untrots <untrots@gmail.com>
2015-09-15 14:50:35 +0300

    +KeyboardBase; +DummyKeyboard

---
+++ projects/RagingBool.Carcosa.Devices_cs/InputControl/DummyKeyboard.cs
@@ -0,0 +1,33 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+using Epicycle.Input.Keyboard;
+using System;
+
+namespace RagingBool.Carcosa.Devices.InputControl
+{
+    public sealed class DummyKeyboard<TKeyId, TAdditionalKeyEventData> : IKeyboard<TKeyId, TAdditionalKeyEventData>
+    {
+        public event EventHandler<KeyEventArgs<TKeyId, TAdditionalKeyEventData>> OnKeyEvent { add { } remove { } }
+
+        public KeyState GetKeyState(TKeyId keyId)
+        {
+            return KeyState.Released;
+        }
+    }
+}
---
+++ projects/RagingBool.Carcosa.Devices_cs/InputControl/KeyboardBase.cs
@@ -0,0 +1,62 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+using Epicycle.Input.Keyboard;
+using System;
+using System.Collections.Generic;
+
+namespace RagingBool.Carcosa.Devices.InputControl
+{
+    public abstract class KeyboardBase<TKeyId, TAdditionalKeyEventData> : IKeyboard<TKeyId, TAdditionalKeyEventData>
+    {
+        private readonly Dictionary<TKeyId, KeyState> _keyStates;
+
+        public KeyboardBase()
+        {
+            _keyStates = new Dictionary<TKeyId, KeyState>();
+        }
+
+        protected void ProcessKeyEvent(KeyEventArgs<TKeyId, TAdditionalKeyEventData> eventArgs)
+        {
+            var keyId = eventArgs.KeyId;
+            var eventType = eventArgs.EventType;
+
+            var prevState = GetKeyState(keyId);
+            var newState = eventType != KeyEventType.Released ? KeyState.Pressed : KeyState.Released;
+
+            _keyStates[keyId] = newState;
+
+            if (OnKeyEvent != null && eventType == KeyEventType.Repeat || prevState != newState)
+            {
+                OnKeyEvent(this, eventArgs);
+            }
+        }
+
+        public event EventHandler<KeyEventArgs<TKeyId, TAdditionalKeyEventData>> OnKeyEvent;
+
+        public KeyState GetKeyState(TKeyId keyId)
+        {
+            if (!_keyStates.ContainsKey(keyId))
+            {
+                return KeyState.Released;
+            }
+
+            return _keyStates[keyId];
+        }
+    }
+}
