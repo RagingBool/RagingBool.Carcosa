f697760b76d6d66cbf0c01f139b1924d07e38077
untrots <untrots@gmail.com>
2015-09-15 18:26:31 +0300

    +BidirectionalMovementKeysConfiguration

---
+++ projects/RagingBool.Carcosa.Devices_cs/InputControl/BidirectionalMovementKeysConfiguration.cs
@@ -0,0 +1,35 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+namespace RagingBool.Carcosa.Devices.InputControl
+{
+    public sealed class BidirectionalMovementKeysConfiguration<TKeyId>
+    {
+        private readonly TKeyId _negativeDirectionKeyId;
+        private readonly TKeyId _positiveDirectionKeyId;
+
+        public BidirectionalMovementKeysConfiguration(TKeyId negativeDirectionKeyId, TKeyId positiveDirectionKeyId)
+        {
+            _negativeDirectionKeyId = negativeDirectionKeyId;
+            _positiveDirectionKeyId = positiveDirectionKeyId;
+        }
+
+        public TKeyId NegativeDirectionKeyId { get { return _negativeDirectionKeyId; } }
+        public TKeyId PositiveDirectionKeyId { get { return _positiveDirectionKeyId; } }
+    }
+}
