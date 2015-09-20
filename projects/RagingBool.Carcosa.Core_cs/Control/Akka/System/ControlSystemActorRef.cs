40f0326b07f548e04fc4877a01088c441dd94496
untrots <untrots@gmail.com>
2015-09-20 22:45:48 +0300

    +ControlSystemActorRef

---
+++ projects/RagingBool.Carcosa.Core_cs/Control/Akka/System/ControlSystemActorRef.cs
@@ -0,0 +1,38 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+using Akka.Actor;
+using System;
+
+namespace RagingBool.Carcosa.Core.Control.Akka.System
+{
+    public class ControlSystemActorRef
+    {
+        private readonly IActorRef _actorRef;
+
+        public ControlSystemActorRef(IActorRef actorRef)
+        {
+            _actorRef = actorRef;
+        }
+
+        public void CreateComponent(string name, Type componentType)
+        {
+            _actorRef.Tell(new CreateComponentMesssage(name, componentType));
+        }
+    }
+}
