1f7e5d3014cef436b0c4a2796e261df10f5e8e0a
untrots <untrots@gmail.com>
2015-09-20 18:56:20 +0300

    +ControlPort

---
+++ projects/RagingBool.Carcosa.Core_cs/Control/ControlPortBase.cs
@@ -0,0 +1,41 @@
+﻿// [[[[INFO>
+// Copyright 2015 Raging Bool (http://ragingbool.org, https://github.com/RagingBool)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// For more information check https://github.com/RagingBool/RagingBool.Carcosa
+// ]]]]
+
+using System;
+
+namespace RagingBool.Carcosa.Core.Control
+{
+    public abstract class ControlPortBase : IControlPort
+    {
+        private readonly IControlComponent _component;
+        private readonly string _name;
+        private readonly Type _dataType;
+
+        public ControlPortBase(IControlComponent component, string name, Type dataType)
+        {
+            _component = component;
+            _name = name;
+            _dataType = dataType;
+        }
+
+        public IControlComponent Component { get { return _component; } }
+        public string Name { get { return _name; } }
+
+        public Type DataType { get { return _dataType; } }
+    }
+}
